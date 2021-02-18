e46f6023cc70024b7d3969a9a7d31695a4afbbd7
preethikishore <preethikishre@gmail.com>
2021-02-18 12:10:33 +0530

    Update

--- app.js
+++ app.js
@@ -60,5 +60,7 @@ app.get('/login',(req,res)=>
 {
     res.render('login');
 })
-app.listen(3000,()=>console.log('Listening 3000'));
+
+let port = process.env.PORT || 3000;
+app.listen(port,()=>console.log(`Listening ${port}`));
 
